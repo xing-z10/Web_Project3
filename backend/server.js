d072b0147edc237b252db4823587399cf44628f2
xing-z10 <zxing1998@gmail.com>
2026-03-19 00:36:47 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ async function start() {
 
   // Serve React frontend
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
   });
 
